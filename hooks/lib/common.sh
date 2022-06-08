ce82846d40d43fdffb0ef7016bcdf3ecb822f93e
André Roaldseth <andre.roaldseth@nav.no>
2022-06-08 11:51:04 +0200

    Prøv med bare en watch-hook

--- hooks/lib/common.sh
+++ hooks/lib/common.sh
@@ -17,17 +17,6 @@ kubernetes:
   namespace:
     nameSelector:
       matchNames: ["teamdagpenger"]
-- name: ConnectStatusMonitor
-  apiVersion: v1
-  kind: ConfigMap
-  executeHookOnEvent: ["Added","Deleted","Modified"]
-  jqFilter: ".metadata.labels.enabled"
-  labelSelector:
-    matchLabels:
-      destination: connect
-  namespace:
-    nameSelector:
-      matchNames: ["teamdagpenger"]
 EOF
 }
 
