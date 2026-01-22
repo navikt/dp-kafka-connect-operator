21dac873c5e75e95680aee2d8688e8d0618d32eb
André Roaldseth <andre.roaldseth@nav.no>
2026-01-22 15:54:07 +0100

    Legg til logging av ktor-events

--- src/main/kotlin/no/nav/dagpenger/kafka/connect/operator/ObservabilityServer.kt
+++ src/main/kotlin/no/nav/dagpenger/kafka/connect/operator/ObservabilityServer.kt
@@ -1,10 +1,18 @@
 package no.nav.dagpenger.kafka.connect.operator
 
 import io.github.oshai.kotlinlogging.KotlinLogging
+import io.ktor.server.application.ApplicationStarted
+import io.ktor.server.application.ApplicationStarting
+import io.ktor.server.application.ApplicationStopPreparing
+import io.ktor.server.application.ApplicationStopped
+import io.ktor.server.application.ApplicationStopping
+import io.ktor.server.application.ServerReady
 import io.ktor.server.application.install
+import io.ktor.server.application.log
 import io.ktor.server.cio.CIO
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.metrics.micrometer.MicrometerMetrics
+import io.ktor.server.plugins.calllogging.CallLogging
 import io.ktor.server.response.respondText
 import io.ktor.server.routing.get
 import io.ktor.server.routing.routing
@@ -24,6 +32,8 @@ class ObservabilityServer(
                 registry = meterRegistry
             }
 
+            install(CallLogging) {}
+
             routing {
                 get("/internal/metrics") {
                     call.respondText(meterRegistry.scrape())
@@ -37,6 +47,13 @@ class ObservabilityServer(
                     call.respondText("READY")
                 }
             }
+
+            monitor.subscribe(ApplicationStarting) { it.log.info("Application starting …") }
+            monitor.subscribe(ApplicationStarted) { it.log.info("Application started …") }
+            monitor.subscribe(ServerReady) { it.log.info("Application ready …") }
+            monitor.subscribe(ApplicationStopPreparing) { it.log.info("Application preparing to stop …") }
+            monitor.subscribe(ApplicationStopping) { it.log.info("Application stopping …") }
+            monitor.subscribe(ApplicationStopped) { it.log.info("Application stopped …") }
         }
 
     fun start() {
